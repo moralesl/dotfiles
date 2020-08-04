c19ce00bfb57852f953f8b18d93c64a51a461e7a
Luis Morales <moralesl@users.noreply.github.com>
2020-08-04 11:46:45 +0200

    Add docker short cuts

--- aliases.zsh
+++ aliases.zsh
@@ -18,3 +18,10 @@ alias ......="cd ../../../../.."
 eval $(thefuck --alias)
 alias fu=fuck
 
+# Docker
+## See: https://blog.baudson.de/blog/stop-and-remove-all-docker-containers-and-images
+alias dkr-stop-all='docker stop $(docker ps -aq)'
+alias dkr-rm-all-containers='docker rm $(docker ps -aq)'
+alias dkr-rm-all-images='docker rmi $(docker images -q)'
+alias dkr-clean-up='docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi $(docker images -q)'
+
