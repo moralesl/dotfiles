292f19de417f2466a2d062c0be422f1d2323c0ea
Luis Morales <moralesl@users.noreply.github.com>
2023-06-27 14:05:38 +0200

    Make backup more resilient

--- sync-to-backup.sh
+++ sync-to-backup.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-# Crontab 0 * * * * $HOME/.dotfiles/sync-to-backup.sh
-RSYNC_OPTIONS="-a --delete --exclude=node_modules/ --exclude=.git/ --exclude=.DS_Store --exclude=.localized"
+# Crontab 0 17,18 * * * $HOME/.dotfiles/sync-to-backup.sh >> $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/replace.log.txt 2>> $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/replace.log.error.txt
+RSYNC_OPTIONS='--archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"}'
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "=========================================="
+echo "=========================================="
+echo "Starting sync at $dt"
+echo "Using rsync options: $RSYNC_OPTIONS"
+echo "=========================================="
 
 # Sync all folders
-rsync $RSYNC_OPTIONS $HOME/Desktop/ $HOME/WorkDocs/Backup/rsync/Desktop
-rsync $RSYNC_OPTIONS $HOME/Documents/ $HOME/WorkDocs/Backup/rsync/Documents
-rsync $RSYNC_OPTIONS $HOME/Downloads/ $HOME/WorkDocs/Backup/rsync/Downloads
-rsync $RSYNC_OPTIONS $HOME/workspace/ $HOME/WorkDocs/Backup/rsync/workspace
-rsync $RSYNC_OPTIONS $HOME/Library/Application\ Support/Firefox/Profiles/838y6sha.default-esr/bookmarkbackups/ $HOME/WorkDocs/Backup/rsync/Application/Firefox
-rsync $RSYNC_OPTIONS $HOME/.gitconfig $HOME/WorkDocs/Backup/rsync/_gitconfig
-rsync $RSYNC_OPTIONS $HOME/.dotfiles/ $HOME/WorkDocs/Backup/rsync/_dotfiles
+echo "--> Sync Desktop files"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"} $HOME/Desktop/ $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/Desktop
+echo "--> Sync Document files"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"} $HOME/Documents/ $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/Documents
+echo "--> Sync Download files"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"} $HOME/Downloads/ $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/Downloads
+echo "--> Sync workspace files"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","build","cdk.out","python*","venv",".aws-sam","bin","vendor"} $HOME/workspace/ $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/workspace
+echo "--> Sync Firefox files"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"} $HOME/Library/Application\ Support/Firefox/Profiles/dqqk24qv.default-release/bookmarkbackups/ $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/Application/Firefox
+echo "--> Sync gitconfig"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"} $HOME/.gitconfig $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch
+echo "--> Sync dotfiles"
+rsync --archive --stats --no-links --exclude={".git",".DS_Store",".localized","node_modules","target","cdk.out"} $HOME/.dotfiles/ $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch/.dotfiles
 
 # Rename dotfiles to _files
-find $HOME/WorkDocs/Backup/rsync -iname ".*" -exec realpath {} \; | rename 's/\.(.*)/_$1/' >> $HOME/WorkDocs/Backup/rsync/replace.log 2>> $HOME/WorkDocs/Backup/rsync/replace.log.txt
+#echo "--> Rename dotfiles"
+#find $HOME/Amazon\ WorkDocs\ Drive/My\ Documents/Backup/rsync-MacBookPro2019-16inch -iname ".*" -exec realpath {} \; | rename -f 's/\.(.*)/_$1/'
+
+
+echo "=========================================="
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "Finished sync at $dt"
+echo "=========================================="
+echo "=========================================="
