9b34622e34328ea709d5cfa8557fc551b2347a0e
Luis Morales <moralesl@users.noreply.github.com>
2023-06-27 14:06:00 +0200

    Make alias more resilient

--- aliases.zsh
+++ aliases.zsh
@@ -4,15 +4,15 @@ alias ohmyzsh="nano ~/.oh-my-zsh"
 alias reloadcli="source $HOME/.zshrc"
 
 # Directories
-alias dotfiles="cd $DOTFILES"
-alias workspace="cd $HOME/workspace"
+alias dotfiles="cd $DOTFILES/"
+alias workspace="cd $HOME/workspace/"
 
 # Directory traversal
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias ......="cd ../../../../.."
+alias ..="cd ../"
+alias ...="cd ../../"
+alias ....="cd ../../../"
+alias .....="cd ../../../../"
+alias ......="cd ../../../../../"
 
 # TheFuck
 eval $(thefuck --alias)
@@ -23,7 +23,7 @@ alias fu=fuck
 alias dkr-stop-all='docker stop $(docker ps -aq)'
 alias dkr-rm-all-containers='docker rm $(docker ps -aq)'
 alias dkr-rm-all-images='docker rmi $(docker images -q)'
-alias dkr-clean-up='docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi $(docker images -q)'
+alias dkr-clean-up='docker stop $(docker ps -aq) || docker rm $(docker ps -aq) || docker rmi $(docker images -q)'
 
 # GGP
 alias ggp='java -jar $HOME/workspace/sandbox/ggp/aws-greengrass-provisioner/build/libs/AwsGreengrassProvisioner.jar'
